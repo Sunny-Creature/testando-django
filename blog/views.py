7135182246692b4ce05c182777d05e2bdc956df7
Sunny-Creature <soniaisissoninha119@gmail.com>
2025-10-23 12:00:11 -0300

    começo de forms e bootstrap

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,22 @@
 from django.shortcuts import render
 from .models import Postagem
+from .forms import PostagemForm
 
 # Create your views here.
 def index(request):
     context = {
         'posts': Postagem.objects.all(),
+        'form': PostagemForm(),
     }
+
+    if request.method == 'POST':
+        form = PostagemForm(request.POST, request.FILES)
+
+        print("é um post!!!!!!!!!!")
+        if form.is_valid():
+            form.save()
+            print("salvou")
+
+        context['form'] = form
+
     return render(request, "blog/index.html", context)
